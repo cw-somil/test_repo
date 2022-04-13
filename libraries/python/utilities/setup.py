dcf14ba7f56da7772be2e4f266875e452b7883ba
Somil <somil@carwale.com>
2022-04-13 16:22:34 +0530

    Update setup.py

--- libraries/python/utilities/setup.py
+++ libraries/python/utilities/setup.py
@@ -1,18 +1,19 @@
-waimport setuptools
+import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+if __name__ == "__main__":
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
 
-setuptools.setup(
-    name='carwale_utilities',
-    version='0.0.1',
-    author='CarWale',
-    author_email='',
-    description='Python utilities for CarWale',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='',
-    license='MIT',
-    packages=['utilities'],
-    install_requires=['boto3>=1.21.39', 'PyYAML>=6.0', 'consulate>=0.6.0'],
-)
+    setuptools.setup(
+        name='carwale_utilities',
+        version='0.0.1',
+        author='CarWale',
+        author_email='',
+        description='Python utilities for CarWale',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='',
+        license='MIT',
+        packages=['utilities'],
+        install_requires=['boto3>=1.21.39', 'PyYAML>=6.0', 'consulate>=0.6.0'],
+    )
